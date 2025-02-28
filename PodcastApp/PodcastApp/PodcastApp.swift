9a74580c1ada5a9921be9c2b76d5ee2a68de0a8a
Jhonata-Jackson <jhonata.jacksondev@gmail.com>
2025-02-28 11:47:46 -0300

    feat: :sparkles: upload the base project

---
+++ PodcastApp/PodcastApp/PodcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastApp.swift
+//  PodcastApp
+//
+//  Created by Jhonata Jackson on 28/02/25.
+//
+
+import SwiftUI
+
+@main
+struct PodcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
